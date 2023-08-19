802ca5337428c58aa0be5f64ca86d692ede8a7e5
Suvankar <suvankar_das@outlook.com>
2023-08-19 14:03:24 +0530

    binary search using array & vector

--- lecture_12.cpp
+++ lecture_12.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int binarySearch(int arr[], int n, int key) {
@@ -6,7 +6,7 @@ int binarySearch(int arr[], int n, int key) {
     int end = n;
     int mid = start + (end - start) / 2;
 
-    while(start <= end) {
+    while (start <= end) {
         if (arr[mid] == key) return mid;
         else if (arr[mid] < key) start = mid+1;
         else end = mid-1;
@@ -17,7 +17,28 @@ int binarySearch(int arr[], int n, int key) {
 }
 
 
+
+int binarySearch(vector<int> vect, int key) {
+    int start = 0;
+    int end = vect.size();
+    int mid = start + (end - start) / 2;
+
+    while (start <= end) {
+        if (vect[mid] == key) return mid;
+        else if (vect[mid] < key) start = mid + 1;
+        else end = mid - 1;
+        mid = start + (end - start) / 2;
+    }
+    
+    return -1;
+}
+
+
+
 int main() {
     int arr[] = {1,2,3,4,5,6,7};
-    cout << binarySearch(arr, sizeof(arr)/sizeof(arr[0]), 5);
+    cout << binarySearch(arr, sizeof(arr)/sizeof(arr[0]), 4) << endl;
+
+    vector<int> vect = {1,2,3,4,5,6,7};
+    cout << binarySearch(vect, 6) << endl;
 }
