caf2034b5378b313eaf80e043604a5fe1a0219fe
Suvankar <suvankar_das@outlook.com>
2023-08-19 15:30:23 +0530

    lecture_13 initial commit

---
+++ lecture_13.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://www.codingninjas.com/studio/problems/first-and-last-position-of-an-element-in-sorted-array_1082549
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
+
+    // FIRST OCC.
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end - start) / 2;
+    int first = -1;
+
+    while(start <= end) {
+        if(arr[mid] == k) {
+            first = mid;
+            end = mid - 1;
+        }
+        else if(arr[mid] < k) start = mid + 1;
+        else end = mid - 1;
+        mid = start + (end - start) / 2;
+    }
+    
+    
+    // LAST OCC.
+    start = 0;
+    end = n-1;
+    mid = start + (end - start) / 2;
+    int last = -1;
+
+    while(start <= end) {
+        if(arr[mid] == k) {
+            last = mid;
+            start = mid + 1;
+        }
+        else if(arr[mid] < k) start = mid + 1;
+        else end = mid - 1;
+        mid = start + (end - start) / 2;
+    }
+
+
+    return {first, last};
+}
