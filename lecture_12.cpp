b813055899db4c576303b5a16b572885ad8747f7
Suvankar <suvankar_das@outlook.com>
2023-08-19 13:31:43 +0530

    revising binary search again

--- lecture_12.cpp
+++ lecture_12.cpp
@@ -2,17 +2,7 @@
 using namespace std;
 
 int binarySearch(int arr[], int n, int key) {
-    int start = 0, end = n - 1;
-    int mid = start + (end - start) / 2;
-
-    while(start <= end) {
-        if(arr[mid] == key) return mid;
-        else if(arr[mid] < key) start = mid + 1;
-        else end = mid - 1; 
-        mid = start + (end - start) / 2;
-    }
-
-    return -1;
+    
 }
 
 int main() {
