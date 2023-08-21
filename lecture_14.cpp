d1095763536ca6f325c90a6d4d5b1f7769f0a583
Suvankar <suvankar_das@outlook.com>
2023-08-21 14:08:09 +0530

    added - Sqrt of a number with precision

--- lecture_14.cpp
+++ lecture_14.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 int findMinInRotatedSorted(vector<int> arr) {
-
     int start = 0;
     int end = arr.size()-1;
     
@@ -40,12 +39,72 @@ int peakInAMountainArray(vector<int> arr) {
 
 
 
+// https://leetcode.com/problems/sqrtx/
+int sqrtOfX(int x) {
+    int start = 0;
+    int end = x;
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+
+    while(start <= end) {
+        if(mid * mid == x) return mid;
+        else if(mid * mid > x) end = mid - 1;
+        else {
+            ans = mid;
+            start = mid + 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+
+    return ans;
+}
+
+
+
+double sqrtWithPrecision(int x, int decimalPlace) {
+
+    // getting base(integer) value
+    int start = 0;
+    int end = x;
+    int mid = start + (end - start) / 2;
+    double ans = -1;
+
+    while(start <= end) {
+        if(mid * mid == x) {
+            ans = mid;
+            break;
+        }
+        else if(mid * mid > x) end = mid - 1;
+        else {
+            ans = mid;
+            start = mid + 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+
+
+    // getting decimal values
+    double factor = 1;
+
+    for(int i = 0; i < decimalPlace; i++) {
+        factor /= 10;
+        for(double j = ans; j*j < x; j+=factor) ans = j;
+    }
+
+    return ans;
+}
+
+
 int main() {
     vector<int> nums;
 
     // nums = {4,5,6,7,0,1,2};
     // cout << findMinInRotatedSorted(nums) << endl;
 
-    nums = {1,2,3,4,5,4,0};
-    cout << peakInAMountainArray(nums) << endl;
+    // nums = {1,2,3,4,5,4,0};
+    // cout << peakInAMountainArray(nums) << endl;
+
+    int n = 63;
+    cout << sqrtOfX(n) << endl;
+    cout << sqrtWithPrecision(n, 5) << endl;
 }
