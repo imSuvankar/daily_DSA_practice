05e34c4d3a248ea41efd8bbe69322a863c10ecc7
Suvankar <suvankar_das@outlook.com>
2023-08-21 13:27:55 +0530

    added - Peak In A Mountain Array

--- lecture_14.cpp
+++ lecture_14.cpp
@@ -23,13 +23,29 @@ int findMinInRotatedSorted(vector<int> arr) {
 
 
 
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+int peakInAMountainArray(vector<int> arr) {
+    int start = 0;
+    int end = arr.size()-1;
+    int mid = start + (end - start) / 2;
 
+    while (start < end) {
+        if (arr[mid] < arr[mid+1]) start = mid + 1;
+        else end = mid;
+        mid = start + (end - start) / 2;
+    }
+
+    return arr[mid];
+}
 
 
 
 int main() {
     vector<int> nums;
 
-    nums = {4,5,6,7,0,1,2};
-    cout << findMinInRotatedSorted(nums);
+    // nums = {4,5,6,7,0,1,2};
+    // cout << findMinInRotatedSorted(nums) << endl;
+
+    nums = {1,2,3,4,5,4,0};
+    cout << peakInAMountainArray(nums) << endl;
 }
