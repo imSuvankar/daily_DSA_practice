1fd3be36689dfe88e9ecab0285a08b9ae4f3e2b8
Suvankar <suvankar_das@outlook.com>
2023-08-25 15:01:57 +0530

    added - Aggressive Cows

--- lecture_15.cpp
+++ lecture_15.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 // https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
-class BookAllocationProblem {
+class BookAllocation {
 private:
     bool isPossible(vector<int> arr, int days, int mid) {
         int currDay = 1;
@@ -87,14 +87,61 @@ public:
 
 
 
+// https://www.codingninjas.com/studio/problems/aggressive-cows_1082559
+class AggresiveCows {
+private:
+    bool isPossible(vector<int> arr, int cows, int mid) 
+    {
+        int currCow = 1;
+        int lastPos = arr[0];
+
+        for(int i : arr) {
+            if(i - lastPos >= mid) {
+                currCow++;
+                if(currCow == cows) return true;
+                lastPos = i;
+            }
+        }
+
+        return false;
+    }
+
+public:
+    int aggressiveCows(vector<int> &stalls, int k)
+    {
+        sort(stalls.begin(), stalls.end());
+        int start = 0;
+        int end = *max_element(stalls.begin(), stalls.end()) - *min_element(stalls.begin(), stalls.end());
+        int mid = start + (end - start) / 2;
+        int ans = -1;
+
+        while (start <= end) {
+            if(isPossible(stalls, k, mid)) {
+                ans = mid;
+                start = mid + 1;
+            }
+            else end = mid - 1;
+            mid = start + (end - start) / 2;
+        }
+
+        return ans;
+    }
+};
+
+
+
 int main() {
     vector<int> arr; int k;
 
     // arr = {3,2,2,4,1,4};
     // k = 3;
-    // cout << BookAllocationProblem().shipWithinDays(arr, k) << endl;
+    // cout << BookAllocation().shipWithinDays(arr, k) << endl;
 
-    arr = {2,1,5,6,2,3};
-    k = 2;
-    cout << PaintersPartition().findLargestMinDistance(arr, k) << endl;
+    // arr = {2,1,5,6,2,3};
+    // k = 2;
+    // cout << PaintersPartition().findLargestMinDistance(arr, k) << endl;
+
+    arr = {0,3,4,7,10,9};
+    k = 4;
+    cout << AggresiveCows().aggressiveCows(arr, k);
 }
